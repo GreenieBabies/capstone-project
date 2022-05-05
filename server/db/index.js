ee4225a157efcb196741af1dbc0f20109cb8ea54
Jeff Lupton <jeffreylupton123@gmail.com>
2022-05-05 11:14:59 -0700

    Initial Model Definition

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,23 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Column = require("./models/Column");
+const Project = require("./models/Project");
+const Task = require("./models/Task");
 
-//associations could go here!
+User.belongsToMany(Column, { through: Project });
+Column.belongsToMany(User, { through: Project });
+Column.hasMany(Task);
+Task.belongsTo(Column);
 
 module.exports = {
   db,
   models: {
     User,
+    Column,
+    Project,
+    Task,
   },
-}
+};
