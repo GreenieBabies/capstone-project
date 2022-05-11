c281b806bd09bc62adc2bc07728d4d7937684adc
Jeff Lupton <jeffreylupton123@gmail.com>
2022-05-11 14:45:48 -0700

    chakra

--- client/components/SingleFounder.js
+++ client/components/SingleFounder.js
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
+import { IconButton } from "@chakra-ui/button"
+import { FaLinkedin, FaGithub } from "react-icons/fa"
 
 const SingleFounder = () => {
   const location = useLocation()
   const state = location.state
+  console.log(state)
 
   return (
     <div className="SingleFounder">
       <img src={state.imageUrl} />
       <p>{`Name: ${state.firstName} ${state.lastName}`}</p>
       <p>{`About Me: ${state.aboutMe}`}</p>
+      <h2>Connect with me!</h2>
+      <IconButton
+        ml="8"
+        icon={<FaLinkedin />}
+        isRound={true}
+        onClick={() => window.open(`${state.linkedin}`, "_blank")}
+      ></IconButton>
+      <IconButton
+        ml="8"
+        icon={<FaGithub />}
+        isRound={true}
+        onClick={() => window.open(`${state.gitHub}`, "_blank")}
+      ></IconButton>
     </div>
   )
 }
