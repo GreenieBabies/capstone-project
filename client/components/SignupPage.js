ad0122b465170ad79b7a346d44b10f1613af7723
MichaelMnatsakanian <michaelmnat@gmail.com>
2022-05-16 06:45:17 -0800

    Created signup page

---
+++ client/components/SignupPage.js
@@ -0,0 +1,107 @@
+import React from "react"
+import { connect } from "react-redux"
+import { authenticate } from "../store"
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react"
+import { useForm } from "react-hook-form"
+
+/**
+ * COMPONENT
+ */
+const AuthForm = (props) => {
+  const { register } = useForm()
+  const { username, email, address, displayName, handleSubmit, error } = props
+
+  return (
+    <div>
+      <Flex width="full" align="center" justifyContent="center">
+        <Box
+          p={8}
+          maxWidth="500px"
+          borderWidth={1}
+          borderRadius={8}
+          boxShadow="lg"
+        >
+          <Box textAlign="center">
+            <Heading>Sign Up</Heading>
+          </Box>
+          <Box my={4} textAlign="left">
+            <form onSubmit={handleSubmit} name={username}>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  type="username"
+                  placeholder="username"
+                  {...register("username")}
+                />
+              </FormControl>
+              <FormControl mt={6}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  placeholder="*******"
+                  {...register("password")}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input
+                  type="email"
+                  placeholder="email"
+                  {...register("email")}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Address</FormLabel>
+                <Input
+                  type="address"
+                  placeholder="address"
+                  {...register("address")}
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                variantcolor="teal"
+                variant="outline"
+                width="full"
+                mt={4}
+              >
+                Sign Up
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
+    </div>
+  )
+}
+
+const mapSignup = (state) => {
+  return {
+    name: "signup",
+    displayName: "Sign Up",
+    error: state.auth.error,
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleSubmit(evt) {
+      console.log(evt)
+      evt.preventDefault()
+      const formName = evt.target.name
+      const username = evt.target.username.value
+      const password = evt.target.password.value
+      dispatch(authenticate(username, password, formName))
+    },
+  }
+}
+
+export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
