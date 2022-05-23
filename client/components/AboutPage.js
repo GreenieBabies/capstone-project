498f7729f25809548bed515fde697ccd973f6f99
Tony Li <tonyxli20@gmail.com>
2022-05-23 08:23:17 -0700

    about page overview

--- client/components/AboutPage.js
+++ client/components/AboutPage.js
@@ -61,13 +61,10 @@ const AboutPage = () => {
         <p>Overview</p>
       </Heading>
       <Container>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco l aboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        TrelloPlus is an interactive project management platform that allows for
+        a visually pleasant experience and smooth collaboration between users.
+        This app includes a drag and drop functionality and speech-to-text
+        feature to add projects. Happy planning!
       </Container>
       <br />
       <Heading>
