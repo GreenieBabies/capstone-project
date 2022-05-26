2cae98db7aaa965782555643fbf28b377dc25b14
Ethan Schindel <erschindel@gmail.com>
2022-05-25 22:33:25 -0400

    removed redux-logger from redux

--- client/store/index.js
+++ client/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
-import { createLogger } from "redux-logger"
+// import { createLogger } from "redux-logger"
 import thunkMiddleware from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 import auth from "./auth"
@@ -9,10 +9,11 @@ import singleProjectReducer from "./singleProject"
 const reducer = combineReducers({
   auth,
   user: singleUserReducer,
-  project: singleProjectReducer
+  project: singleProjectReducer,
 })
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+  applyMiddleware(thunkMiddleware) // for deployment
+  // applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
 const store = createStore(reducer, middleware)
 
